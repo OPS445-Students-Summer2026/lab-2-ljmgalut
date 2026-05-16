51a5bf04c1ae7be1663ea3ad14b9426537e15050
Liezel Janine Galut <your_email@example.com>
2026-05-15 22:47:18 -0400

    Completed Lab 2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Liezel Janine Galut
+# Author ID: ljmgalut
+# Date Created: 2026/05/15
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Liezel Janine Galut
+# Author ID: ljmgalut
+# Date Created: 2026/05/15
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
+
